237b99f0cbebd3d65a6030f4cbc0e961cda8611c
zoro.qi <zhangqi560832@163.com>
2020-01-30 14:38:28 +0800

    图标

--- random-money/main.go
+++ random-money/main.go
@@ -25,17 +25,19 @@ func fixedRatioBet(money int64) int64 {
 func main() {
 	rand.Seed(time.Now().Unix())
 	humanNums := 100
-	roll := 10000
-	gh := &gamblingHouse{peoples: make([]gambler, humanNums)}
-
-	humans := make([]*people, humanNums)
-
+	roll := 5000
 	worker := func() int64 {
-		return 2
+		return 0
 	}
+	gambleFund := maxBet
+	for i := 0; i < 1; i++ {
+		gameStart(humanNums, roll, worker, gambleFund)
+	}
+}
 
-	gambleFund := fixedRatioBet
-
+func gameStart(humanNums int, roll int, worker func() int64, gambleFund func(selfMoney int64) int64) {
+	gh := &gamblingHouse{peoples: make([]gambler, humanNums)}
+	humans := make([]*people, humanNums)
 	for i := 0; i < humanNums; i++ {
 		p := &people{money: initMoney, id: i, worker: worker, gambleFund: gambleFund}
 		humans[i] = p
@@ -48,23 +50,20 @@ func main() {
 		gh.apportion()
 		gh.reset()
 	}
-
 	for _, h := range humans {
 		h.keepAccounts()
 	}
-
 	sort.Slice(humans, func(i, j int) bool {
 		return humans[i].money <= humans[j].money
 	})
-
 	printNum := 5
 	num := make([]int, printNum*2)
 	for i := 0; i < printNum; i++ {
 		num[i] = i
 		num[printNum*2-i-1] = len(humans) - i - 1
 	}
-	roll = len(humans[0].account)
-	for i := 0; i < roll; i++ {
+	accountLength := len(humans[0].account) - 1
+	for i := 0; i <= accountLength; i++ {
 		for _, v := range num {
 			fmt.Printf("%d\t", humans[v].account[i].money)
 		}
